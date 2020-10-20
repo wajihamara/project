c3ba43eb6a3f4f6c7be6775af40277b5fe735151
wajihamara <wajihamara@yahoo.com>
2020-10-20 11:27:02 +0100

    shop

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ $('.btn').mouseenter(()=>{
 $('.btn').mouseleave(()=>{
 	$('.btn').css('background-color','#4CAF50')
 })
+ 
+
+
+ 
+ function ValidateEmail(inputText )
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+ 
+
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+ 
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+ 
+return false;
+}
+
+
+
+
+
+}
